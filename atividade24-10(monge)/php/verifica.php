616f45a622889978bc9365a5e4475d91fe81c293
heitorda <heitordestroalbino@gmail.com>
2024-10-24 12:03:34 -0300

    criei a verficação do cadastro

---
+++ atividade24-10(monge)/php/verifica.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verifica</title>
+</head>
+<body>
+    <!-- inicio do PHP -->
+    <?php 
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $cidade= $_POST["cidade"];
+    $estado= $_POST["estado"];
+    $comentarios= $_POST["comentarios"];
+    $erro= 0;
+
+    // verifica se o campo não está em branco
+    if (empty($nome) or strstr ($nome, ' ') ==FALSE)
+    {echo "Favor digitar seu nome corretamente. <br>"; $erro=1; }
+    if (empty($email)<8 || strstr ($email, '@') ==FALSE)
+    {echo "Favor digitar seu email corretamente. <br>"; $erro=1; }
+    if (empty($cidade))
+    {echo "Favor digitar seu sua cidade corretamente. <br>"; $erro=1; }
+    if (empty($estado) or strstr ($nome, ' ') ==FALSE)
+    {echo "Favor digitar seu nome corretamente. <br>"; $erro=1; }
+    if (empty($comentarios) or strstr ($nome, ' ') ==FALSE)
+    {echo "Favor digitar seu nome corretamente. <br>"; $erro=1; }
+  
+
+    ?>
+    <!-- fim do PHP -->
+</body>
+</html>
